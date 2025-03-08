75209a3396be5e33e4f53f7de442562397dd61aa
andrd-jhon <jv-andrade02@outlook.com>
2025-03-08 16:55:42 -0300

    Criação componente Home e Header

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    RouterOutlet
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'landing-page';
+}
